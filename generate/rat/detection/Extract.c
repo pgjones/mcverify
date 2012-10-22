6df75d0354d15ca9a38df29340569f28e5cc1731
Philip.Jones <p.g.jones@qmul.ac.uk>
2012-10-22 23:02:41 +0100

    Improved pmtresponse and nhit extraction from detection data.

--- generate/rat/detection/Extract.c
+++ generate/rat/detection/Extract.c
@@ -8,5 +8,15 @@
   ExtractAveOccupancy( "PB500.root", "PBX500.root" );
   ExtractAveOccupancy( "PB620.root", "PBX620.root" );
 
+  gSystem->Unload( "$MCVERIFY/extract/rat/ExtractAveOccupancy_cc.so" );
+  gROOT->ProcessLine( ".L $MCVERIFY/extract/rat/ExtractNhit.cc+" );
+
+  ExtractNhit( "PB337.root", "PBN337.root" );
+  ExtractNhit( "PB365.root", "PBN365.root" );
+  ExtractNhit( "PB386.root", "PBN386.root" );
+  ExtractNhit( "PB420.root", "PBN420.root" );
+  ExtractNhit( "PB500.root", "PBN500.root" );
+  ExtractNhit( "PB620.root", "PBN620.root" );        
+
   exit;
 }
